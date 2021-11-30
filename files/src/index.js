0800cbde37470b805b61b68ad362bdfc64b68cc6
Vladimir <82714232+junjun-it-courses@users.noreply.github.com>
2021-11-30 19:53:02 +0200

    init

---
+++ files/src/index.js
@@ -0,0 +1,11 @@
+// Эта функция переворачивает переданную строку
+export default str => str.split('').reverse().join(''); // без ошибок
+
+
+// export default str => {
+//     return str
+//             .split('-')                 // ошибка
+//             .reverse()
+//             .join('')
+// }
+
